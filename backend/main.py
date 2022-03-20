d9c80bb916daa68adfc1dc35dab02a1fa0e8801c
Andreas Remar <andreas.remar@gmail.com>
2022-03-20 16:26:15 +0100

    Add logger

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,14 @@
+import logging
 from flask import Flask, request
 
 app = Flask(__name__)
 
+gunicorn_logger = logging.getLogger('gunicorn.error')
+if gunicorn_logger.handlers:
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
+
 @app.route("/api/google-login", methods = ["POST"])
 def google_login():
-    print(request.data.decode("utf-8"))
+    app.logger.warning(request.data.decode("utf-8"))
     return "hejsan"
