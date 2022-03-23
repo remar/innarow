a08f9087df916a7757161cff3d2fa29e5fc8a778
Andreas Remar <andreas.remar@gmail.com>
2022-03-23 17:05:30 +0100

    Don't include .flaskenv when packaging

--- package.sh
+++ package.sh
@@ -5,6 +5,5 @@ mkdir -p ../innarow-build
 cp -r frontend ../innarow-build
 mkdir -p ../innarow-build/backend
 cp backend/*.py ../innarow-build/backend
-cp backend/.flaskenv ../innarow-build/backend
 cp backend/setupenv.sh ../innarow-build/backend
 cp backend/requirements.txt ../innarow-build/backend
