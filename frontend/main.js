24f521a4db36a27e3bf588f8012e392fd5ab7050
Andreas Remar <andreas.remar@gmail.com>
2022-04-09 19:21:46 +0200

    Extract javascript

---
+++ frontend/main.js
@@ -0,0 +1,52 @@
+function createNewGame() {
+    fetch("/api/games", {
+        method: "POST"
+    }).then(() => listGames());
+}
+function listGames() {
+    fetch("/api/games").then(response => {
+        if (response.ok) {
+            response.json().then(gamesObject => {
+                const gameList = document.getElementById("game_list");
+                gameList.innerHTML = "";
+                for (const element of gamesObject.games) {
+                    const li = document.createElement("li");
+                    const button = document.createElement("button");
+                    button.innerHTML = element;
+                    li.appendChild(button);
+                    gameList.appendChild(li);
+                }
+            });
+        }
+    });
+}
+function handleCredentialResponse(response) {
+    console.log(response.credential);
+    fetch("/api/google-login", {
+        method: "POST",
+        body: response.credential
+    }).then(response => console.log(response));
+}
+function getEmail() {
+    return document.getElementById("show_email").innerHTML;
+}
+function setEmail(email) {
+    document.getElementById("show_email").innerHTML = email;
+}
+function setEmailLoop() {
+    if (!getEmail()) {
+        fetch("/api/get-email").then(response => {
+            if (response.ok) {
+                response.text().then(email => setEmail(email));
+            }
+        });
+        setTimeout(setEmailLoop, 1000);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    setEmailLoop();
+
+    // // this function runs when the DOM is ready, i.e. when the document has been parsed
+    // document.getElementById("user-greeting").textContent = "Welcome back, Bart";
+});
